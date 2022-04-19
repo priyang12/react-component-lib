ca243240057f6ba8c574d2d9f3a3e24e701027d3
Priyang <patelpriyang95@gmail.com>
2022-04-20 00:33:16 +0530

    Gradient Button

--- src/lib/components/GradientHover/GradientHover.jsx
+++ src/lib/components/GradientHover/GradientHover.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./GradientHover.scss";
 
-const GradientHover = ({ children }) => {
+const GradientHover = ({ children, animationTime }) => {
   return (
-    <div className="gradient-hover">
+    <div
+      className="gradient-layer"
+      style={{
+        animationDuration: `${animationTime}`,
+      }}
+    >
       <div className="gradient-hover__content">{children}</div>
     </div>
   );
--- src/lib/components/GradientHover/GradientHover.stories.js
+++ src/lib/components/GradientHover/GradientHover.stories.js
@@ -2,20 +2,40 @@ import { Default as Button } from "../Button/Button.stories";
 import GradientHover from "./GradientHover";
 
 export default {
-  title: "Hover/GradientHover",
+  title: "GradientHover/Border",
   component: GradientHover,
-  decorators: [(story) => <div className="container">{story()}</div>],
+  args: {
+    children: (
+      <Button
+        style={{
+          background: "transparent",
+        }}
+      />
+    ),
+    animationTime: "0.2s",
+  },
+  decorators: [
+    (story) => (
+      <div
+        className="container"
+        style={{
+          height: "100px",
+          padding: "20px",
+          backgroundColor: "#f5f5f5",
+        }}
+      >
+        {story()}
+      </div>
+    ),
+  ],
 };
 
 const Template = (args) => <GradientHover {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {
-  children: (
-    <Button
-      style={{
-        background: "transparent",
-      }}
-    />
-  ),
+
+export const Faster = Template.bind({});
+
+Faster.args = {
+  animationTime: "0.1s",
 };
