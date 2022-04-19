244d76567b9c9d6930fa058e3936301aef431dd8
Priyang <patelpriyang95@gmail.com>
2022-04-19 20:07:07 +0530

    start

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import "../src/lib/styles/main.css";
+import "../src/lib/styles/Global.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
