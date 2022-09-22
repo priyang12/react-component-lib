e871abaf015c9113da93ae9841fc773c2c2ca8fa
Priyang <patelpriyang95@gmail.com>
2022-09-23 00:12:51 +0530

    drag screen
    drag parent boundary

--- src/lib/Effects/Draggable/Draggable.tsx
+++ src/lib/Effects/Draggable/Draggable.tsx
@@ -15,39 +15,76 @@ function Draggable({
    children,
 }: DraggableProps) {
    const [position, setPosition] = React.useState({ x: 0, y: 0 });
+   const [PreviousTouch, setPreviousTouch] = React.useState({ x: 0, y: 0 });
 
-   const ref = React.useRef<null | HTMLDivElement>(null);
+   const DragRef = React.useRef<null | HTMLDivElement>(null);
+
    const handleMouseDown = React.useCallback(
       (e: React.MouseEvent<HTMLDivElement>) => {
          e.preventDefault();
          setIsDragging(true);
-         if (ref.current) {
-            ref.current.style.cursor = 'grabbing';
+         if (DragRef.current) {
+            DragRef.current.style.cursor = 'grabbing';
          }
       },
-      [setIsDragging, ref]
+      [setIsDragging, DragRef]
    );
 
    const handleMouseUp = React.useCallback(
       (e: React.MouseEvent<HTMLDivElement>) => {
          e.preventDefault();
          setIsDragging(false);
-         if (ref.current) {
-            ref.current.style.cursor = 'grab';
+         if (DragRef.current) {
+            DragRef.current.style.cursor = 'grab';
          }
       },
-      [setIsDragging, ref]
+      [setIsDragging, DragRef]
    );
 
    const handleMouseMove = React.useCallback(
       (e: React.MouseEvent<HTMLDivElement>) => {
          if (isDragging) {
-            if (parentContainerRef?.current) {
-               // if parent ref is passed than it should not drag out side the parent container
-               const parentContainer = parentContainerRef.current;
-               const parentContainerRect = parentContainer.getBoundingClientRect();
+            setPosition({
+               x: position.x + e.movementX,
+               y: position.y + e.movementY,
+            });
+         }
+      },
+      [isDragging, position]
+   );
 
-               // still need to implement it.
+   const handleMouseMoveWithParent = React.useCallback(
+      (e: React.MouseEvent<HTMLDivElement>) => {
+         if (isDragging) {
+            if (parentContainerRef?.current && DragRef.current) {
+               // if parent ref is passed than it should not drag out side the parent container
+               const parentRect = parentContainerRef.current.getBoundingClientRect();
+               const drag = DragRef.current?.getBoundingClientRect();
+
+               if (drag.top < parentRect.top) {
+                  setPosition({
+                     ...position,
+                     y: 0,
+                  });
+               } else if (drag.x < parentRect.x) {
+                  setPosition({
+                     ...position,
+                     x: 0,
+                  });
+               } else if (
+                  drag.bottom - parentRect.bottom > parentRect.bottom / 2 &&
+                  e.movementY > 0
+               ) {
+                  setPosition({
+                     ...position,
+                     y: position.y,
+                  });
+               } else {
+                  setPosition({
+                     x: position.x + e.movementX,
+                     y: position.y + e.movementY,
+                  });
+               }
             } else {
                setPosition({
                   x: position.x + e.movementX,
@@ -59,14 +96,46 @@ function Draggable({
       [isDragging, parentContainerRef, position]
    );
 
+   const handleTouchStart = React.useCallback(
+      (e: React.TouchEvent<HTMLDivElement>) => {
+         setIsDragging(true);
+         if (DragRef.current) {
+            DragRef.current.style.cursor = 'grabbing';
+         }
+      },
+      [setIsDragging, DragRef]
+   );
+
+   const handleTouchMove = React.useCallback(
+      (e: React.TouchEvent<HTMLDivElement>) => {
+         if (isDragging) {
+            setPreviousTouch({
+               x: e.touches[0].pageX,
+               y: e.touches[0].pageY,
+            });
+            if (PreviousTouch.x !== 0 && PreviousTouch.y !== 0) {
+               setPosition({
+                  x: position.x + e.touches[0].pageX - PreviousTouch.x,
+                  y: position.y + e.touches[0].pageY - PreviousTouch.y,
+               });
+            }
+         }
+      },
+      [isDragging, position]
+   );
+
    return (
       <div>
          <div
-            ref={ref}
+            ref={DragRef}
             onMouseDown={handleMouseDown}
             onMouseUp={handleMouseUp}
             onMouseLeave={() => setIsDragging(false)}
-            onMouseMove={handleMouseMove}
+            onMouseMove={
+               parentContainerRef ? handleMouseMoveWithParent : handleMouseMove
+            }
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
             style={{
                transform: `translate(${position.x}px, ${position.y}px)`,
                cursor: 'grab',
