687a636fb39609f233c361b8ef5277ee624c12f7
Priyang <patelpriyang95@gmail.com>
2025-06-18 17:48:23 +0530

    removed menuList

--- src/lib/Module/MenuList/MenuList.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { composeStories } from '@storybook/react';
-import * as MenuListStories from './MenuList.stories';
-const { Template } = composeStories(MenuListStories);
-
-it('should render without crashing', () => {
-   render(<Template />);
-});
