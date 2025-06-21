a516afb7572b492c866afcb6df3c9c1888c141b6
Priyang <patelpriyang95@gmail.com>
2025-06-21 19:29:11 +0530

    example playground fixed over riding classes seems to works

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import { Button, Ring } from 'react-component-lib';
+import { FormControl, Input, Label } from 'react-component-lib';
 import 'react-component-lib/dist/index.css';
 
 // import '../../dist/index.css';
@@ -11,29 +11,12 @@ function App() {
 
    return (
       <div className="App">
-         <Ring>
-            <Button>asdsad</Button>
-         </Ring>
-         <div>
-            <a href="https://vitejs.dev" target="_blank">
-               <img src="/vite.svg" className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://reactjs.org" target="_blank">
-               <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-         </div>
-         <h1>Vite + React</h1>
-         <div className="card">
-            <button onClick={() => setCount(count => count + 1)}>
-               count is {count}
-            </button>
-            <p>
-               Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-         </div>
-         <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-         </p>
+         <FormControl className="flex flex-col">
+            <Label htmlFor="Search" size="medium">
+               Search
+            </Label>
+            <Input type="text" id="Search" InputSize="medium" />
+         </FormControl>
       </div>
    );
 }
