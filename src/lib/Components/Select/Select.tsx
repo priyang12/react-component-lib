ba61cc6d3e185fdfa51a8aa9c7bf95862cac3bfc
Priyang <patelpriyang95@gmail.com>
2025-06-23 19:11:12 +0530

    fixed Select

--- src/lib/Components/Select/Select.tsx
+++ src/lib/Components/Select/Select.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { optionType, useSelect } from '../../../Hooks/useSelect';
+import { optionType, useSelect } from './hooks/useSelect';
 import { useToggle } from '../../../Hooks/useToggle';
 import NativeSelect from './NativeSelect';
 import { Input } from '../Input';
 import { InputProps } from '../Input/Input';
 import { callAll } from '../../Utils/AllFunctionsCall';
 import './Select.scss';
+import { useFormContext } from '../FormControl/FormControl';
 
 export type renderLabelProps = {
    searching: boolean;
@@ -13,9 +14,12 @@ export type renderLabelProps = {
 };
 
 export type renderOptionsProps = {
+   focusedIndex: number;
    filteredOptions: optionType[];
+   selectedOption: optionType | undefined;
    selectValue: (value: string) => void;
    toggle: () => void;
+   setFocusedIndex: (value: number) => void;
 };
 export interface SelectProps {
    initialValue: string;
@@ -42,24 +46,54 @@ function Select({
    renderLabel = () => <label htmlFor={name}>{name}</label>,
    ...props
 }: React.ComponentPropsWithoutRef<'select'> & SelectProps) {
+   const wrapperRef = React.useRef<HTMLDivElement>(null);
    const [isOpen, toggle, setToggle] = useToggle(false);
 
    const {
       value,
-
       searching,
       searchTerm,
+      selectedOption,
+      focusedIndex,
       selectValue,
       onSearchChange,
+      onKeyDown,
+      setFocusedIndex,
       options: filteredOptions,
    } = useSelect({
       initialValue: initialValue,
       options: options,
    });
 
+   const selectInputRef = React.useRef<HTMLDivElement>(null);
+
+   React.useEffect(() => {
+      function handleClickOutside(event: MouseEvent) {
+         if (
+            wrapperRef.current &&
+            !wrapperRef.current.contains(event.target as Node)
+         ) {
+            setToggle(false);
+         }
+      }
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+         document.removeEventListener('mousedown', handleClickOutside);
+      };
+   }, [setToggle]);
+
+   React.useEffect(() => {
+      const node = selectInputRef.current;
+      if (!node) return;
+      node.addEventListener('keydown', onKeyDown);
+      return () => {
+         node.removeEventListener('keydown', onKeyDown);
+      };
+   }, []);
+
    return (
       <>
-         <div className="select-container">
+         <div className="select-container" ref={wrapperRef}>
             {renderLabel({ searching, selectedValue: value })}
             {/* memo this later and pass value using ref */}
             <NativeSelect
@@ -68,20 +102,30 @@ function Select({
                data-value={value}
                {...props}
             />
-            <div className="select-input">
+            <div className="select-input" ref={selectInputRef}>
                <Input
                   id={`${name}-input`}
                   name={name}
-                  value={searchTerm}
+                  value={searching ? searchTerm : selectedOption?.label}
                   data-value={value}
                   aria-expanded={isOpen}
                   onChange={callAll(onSearchChange, props.onChange)}
                   onClick={() => setToggle(true)}
                   InputSize={inputSize}
+                  aria-haspopup="listbox"
+                  aria-controls="custom-select-listbox"
+                  // aria-activedescendant={
+                  //    focusedIndex >= 0
+                  //       ? `custom-option-${filteredOptions[focusedIndex].value}`
+                  //       : undefined
+                  // }
                />
                {isOpen
                   ? renderOptions({
+                       focusedIndex,
+                       setFocusedIndex,
                        filteredOptions,
+                       selectedOption,
                        selectValue,
                        toggle,
                     })
