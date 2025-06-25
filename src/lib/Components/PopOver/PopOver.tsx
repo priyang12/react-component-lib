493b7e29927f070d84f8589c33debc0783c02a97
Priyang <patelpriyang95@gmail.com>
2025-06-25 17:49:04 +0530

    added positioning using floating-ui. for not passing it from external not inside component

--- src/lib/Components/PopOver/PopOver.tsx
+++ src/lib/Components/PopOver/PopOver.tsx
@@ -9,7 +9,7 @@ import PopPortal from './components/PopPortal';
 // Break it into clear subcomponents:
 // Popover: Context provider (state, positioning)
 // PopoverTrigger: The element that opens/closes the popover
-// PopoverContent: The Content element (need to add floating with portal and floating-ui)
+// PopoverContent: The Content element.
 // (Need to add) PopoverArrow, PopoverHeader, PopoverFooter for styling clarity
 // Triggered options by click, hover, focus.
 // might add control state pass later.
