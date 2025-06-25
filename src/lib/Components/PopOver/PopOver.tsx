153ef85b75c9aa4d90dbd488c59b499fcacd8f67
Priyang <patelpriyang95@gmail.com>
2025-06-25 17:31:13 +0530

    make portal model

--- src/lib/Components/PopOver/PopOver.tsx
+++ src/lib/Components/PopOver/PopOver.tsx
@@ -4,31 +4,18 @@ import PopContent from './components/PopContent';
 import PopTrigger from './components/PopTrigger';
 import PopClose from './components/PopClose';
 import './PopOver.scss';
+import PopPortal from './components/PopPortal';
 
 // Break it into clear subcomponents:
 // Popover: Context provider (state, positioning)
 // PopoverTrigger: The element that opens/closes the popover
 // PopoverContent: The Content element (need to add floating with portal and floating-ui)
 // (Need to add) PopoverArrow, PopoverHeader, PopoverFooter for styling clarity
-
-// function PopOver({ children, ...props }: PopOverProps) {
-//    return (
-//       <PopContainer {...props}>
-//          <PopTrigger>
-//             <Button>Open</Button>
-//          </PopTrigger>
-//  <PopContent >
-//   Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-//        placeat labore impedit in saepe, atque tempora aspernatur
-//        consequuntur iure unde velit odio nulla harum mollitia rerum quo
-//        adipisci ipsam sequi.
-//        <PopClose />
-//   </PopContent>
-//       </PopContainer>
-//    );
-// }
+// Triggered options by click, hover, focus.
+// might add control state pass later.
 
 const PopOver = {
+   PopOverPortal: PopPortal,
    Trigger: PopTrigger,
    Content: PopContent,
    Container: PopContainer,
