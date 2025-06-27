ab9c7bc2d16c3f4d1e5ee6f35ccb66f56b036296
Priyang <patelpriyang95@gmail.com>
2025-06-27 23:14:58 +0530

    per-component Bundling and global.css changed the build into three process into three - build use tsup to build the component and single index - build global.css from global.scc - build type using tsc

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,28 +1,82 @@
 import { defineConfig } from 'tsup';
 import { sassPlugin } from 'esbuild-sass-plugin';
 import postcss from 'postcss';
-import tailwind from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import glob from 'fast-glob';
 
-export default defineConfig({
-   entry: ['src/index.tsx'],
+const componentEntries = glob.sync(
+   [
+      'src/lib/Components/**/index.@(ts|tsx)',
+      'src/lib/Wrappers/**/index.@(ts|tsx)',
+      'src/lib/Utils/**/*.@(ts|tsx)',
+   ],
+   {
+      absolute: false,
+      ignore: ['**/Components/index.ts', '**/Wrappers/index.ts'],
+   }
+);
+
+const perComponent = {
+   entry: componentEntries,
+   outDir: 'dist/lib',
+   format: ['esm', 'cjs'],
+   dts: false,
+   clean: true,
+   splitting: false,
+   sourcemap: false,
+   minify: false,
+   external: ['react', 'react-dom'],
    esbuildPlugins: [
       sassPlugin({
-         async transform(source, resolveDir) {
-            const { css } = await postcss([tailwind(), autoprefixer]).process(
-               source
-            );
-            return css;
+         async transform(source) {
+            const result = await postcss([autoprefixer()]).process(source, {
+               from: undefined,
+            });
+            return result.css;
          },
       }),
    ],
-   tsconfig: './tsconfig.json',
-   treeshake: true,
-   minify: true,
-   clean: true,
-   metafile: true,
-   dts: true,
-   splitting: false,
-   format: ['cjs', 'esm'],
-   external: ['react', 'react-dom'],
-});
+} as any;
+
+const globalCss = {
+   entry: ['src/lib/styles/Global.scss'], // Standalone entry
+   outDir: 'dist',
+   dts: false,
+   format: [], // no JS
+   esbuildPlugins: [
+      sassPlugin({
+         async transform(source) {
+            const result = await postcss([autoprefixer()]).process(source, {
+               from: undefined,
+            });
+            return result.css;
+         },
+      }),
+   ],
+};
+
+export default defineConfig([
+   perComponent,
+
+   // All inside main /index.js
+   {
+      entry: ['src/index.tsx'],
+      outDir: 'dist',
+      format: ['esm', 'cjs'],
+      dts: false, // created separated build.
+      splitting: false,
+      sourcemap: true,
+      minify: true,
+      external: ['react', 'react-dom'],
+      esbuildPlugins: [
+         sassPlugin({
+            async transform(source) {
+               const result = await postcss([autoprefixer()]).process(source, {
+                  from: undefined,
+               });
+               return result.css;
+            },
+         }),
+      ],
+   },
+]);
