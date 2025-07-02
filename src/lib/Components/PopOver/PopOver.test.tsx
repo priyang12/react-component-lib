3bd3d0030a40c833c2e7ee12bcce710e74c06963
Priyang <patelpriyang95@gmail.com>
2025-07-02 10:13:50 +0530

    fixed tests

--- src/lib/Components/PopOver/PopOver.test.tsx
+++ src/lib/Components/PopOver/PopOver.test.tsx
@@ -2,5 +2,42 @@ import { render } from '@testing-library/react';
 import PopOver from './PopOver';
 
 it('renders without crashing', () => {
-  render(<PopOver />);
+   render(
+      <PopOver.Container isFlip={true}>
+         <PopOver.Trigger trigger={['focus', 'click']}>Filter</PopOver.Trigger>
+         <PopOver.Portal>
+            <PopOver.Content className="w-1/2">
+               <div className="flex flex-col bg-[var(--bg-surface)] p-4 rounded-lg z-30 space-y-4 ">
+                  <PopOver.Arrow />
+                  <PopOver.Close className="absolute top-2 right-2" />
+                  <PopOver.Header className="text-lg font-semibold text-[var(--text-primary)]">
+                     Popover Title
+                  </PopOver.Header>
+                  <div className="text-[var(--text-primary)] text-sm">
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Debitis placeat labore impedit in saepe, atque tempora
+                     aspernatur consequuntur iure unde velit odio nulla harum
+                     mollitia rerum quo adipisci ipsam sequi.
+                  </div>
+
+                  <PopOver.Footer>
+                     {({ setContentState }) => (
+                        <div className="flex justify-end space-x-2">
+                           <button
+                              onClick={() => setContentState(false)}
+                              className="px-3 py-1 bg-gray-200 rounded"
+                           >
+                              Cancel
+                           </button>
+                           <button className="px-3 py-1 bg-blue-600 text-white rounded">
+                              Confirm
+                           </button>
+                        </div>
+                     )}
+                  </PopOver.Footer>
+               </div>
+            </PopOver.Content>
+         </PopOver.Portal>
+      </PopOver.Container>
+   );
 });
--- src/lib/Components/TextLanding/TextLanding.test.tsx
+++ src/lib/Components/TextLanding/TextLanding.test.tsx
@@ -1,6 +1,5 @@
 import TextLanding from './TextLanding';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 
 it('should render without crashing', () => {
    render(<TextLanding />);
