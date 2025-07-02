c01044a8eeb2bd821486531e17748ba4c812ae20
Priyang <patelpriyang95@gmail.com>
2025-07-02 11:25:11 +0530

    removed DatePicker

--- src/lib/Components/DatePicker/DatePicker.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { composeStories } from '@storybook/react';
-import * as DatePickerStories from './DatePicker.stories';
-const { Template } = composeStories(DatePickerStories);
-
-it('should render without crashing', () => {
-   render(<Template />);
-});
