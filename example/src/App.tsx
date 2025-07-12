11fe8875905f9ec3a71b3f0d54deaf8a17d79e7e
Priyang <patelpriyang95@gmail.com>
2025-07-12 18:10:28 +0530

    change playground

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,22 +1,38 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import { FormControl, Input, Label } from 'react-component-lib';
 import 'react-component-lib/dist/index.css';
 
-// import '../../dist/index.css';
+import { Button } from 'react-component-lib/dist/lib/Components/Button';
+import {
+   Badge,
+   BadgeContainer,
+} from 'react-component-lib/dist/lib/Components/Badge';
+import 'react-component-lib/dist/Components/Badge/index.css';
+import 'react-component-lib/dist/Components/Button/index.css';
+
+// global css
+import 'react-component-lib/dist/global.css';
 
 function App() {
-   const [count, setCount] = useState(0);
-   const [selectedDate, setSelectedDate] = React.useState(new Date());
-
    return (
-      <div className="App">
-         <FormControl className="flex flex-col">
+      <div className="flex flex-col items-center justify-center w-[100vw] h-full">
+         {/* <FormControl className="flex flex-col">
             <Label htmlFor="Search" size="medium">
                Search
             </Label>
             <Input type="text" id="Search" InputSize="medium" />
-         </FormControl>
+            <Button text="Submit" variant="primary" isLoading={false} />
+         </FormControl> */}
+
+         <BadgeContainer>
+            <div className="p-5 border-2 border-solid border-red-400 bg-[var(--bg-surface)]">
+               <Badge
+                  anchorOriginHorizontal="left"
+                  anchorOriginVertical="top"
+                  BadgeContent="1"
+               />
+               <p>container</p>
+            </div>
+         </BadgeContainer>
       </div>
    );
 }
